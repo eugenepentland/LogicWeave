35e79532381d1410b1cdfc800d4b50a3dd5c09b4
Eugene Pentland <epentland@2yfv.com>
2025-11-27 03:05:59 +0000

    added weblogicweave core

--- software/src/LogicWeave/__init__.py
+++ software/src/LogicWeave/__init__.py
@@ -1,4 +1,4 @@
 from .device import LogicWeave, GPIO, UART, I2C, SPI
-from .web import WebLogicWeave
+from .web import WebLogicWeave, WebLogicWeaveCore
 from .logicweave_core import LogicWeaveCore
 from .proto_gen import logicweave_pb2, logicweave_core_pb2
