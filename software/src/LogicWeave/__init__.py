3efc177967764d0be0d107936037fc18391d8bec
Eugene Pentland <epentland@2yfv.com>
2025-11-27 02:44:22 +0000

    removed unused imports

--- software/src/LogicWeave/__init__.py
+++ software/src/LogicWeave/__init__.py
@@ -1,4 +1,4 @@
 from .device import LogicWeave, GPIO, UART, I2C, SPI
 from .web import WebLogicWeave
-from .logicweave_core import LogicWeaveCore, AP33772S
-from .proto_gen import logicweave_pb2
+from .logicweave_core import LogicWeaveCore
+from .proto_gen import logicweave_pb2, logicweave_core_pb2
